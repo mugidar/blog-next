093bd3d9874320d10404d829f1449ed489e1bb76
mugidar <mugidar@gmail.com>
2023-09-13 15:28:31 +0300

    init

---
+++ src/app/page.jsx
@@ -0,0 +1,20 @@
+import CategoryList from '@/components/CategoryList/CategoryList'
+import Featured from '@/components/Featured/Featured'
+import React from 'react'
+import styles from "./page.module.css"
+import CardList from '@/components/CardList/CardList'
+import Menu from '@/components/Menu/Menu'
+const page = () => {
+	return (
+	<div>
+		<Featured/>
+		<CategoryList/>
+		<div className={styles.container}>
+		<Menu/>
+		<CardList/>
+		</div>
+	</div>
+	)
+}
+
+export default page
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from './Navbar.module.css'
+
+const Navbar = () => {
+	return <div className={styles.container}>
+		<div></div>
+		<div></div>
+		<div></div>
+		<div></div>
+		 
+	</div>
+}
+
+export default Navbar
